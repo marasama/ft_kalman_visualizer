4575ebbe07ab8d7d735747b2026ed4e00d06c0dd
Zart Zort <abdullah.durusoy31@gmail.com>
2026-06-27 02:08:15 +0300

    Trying to learn

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,54 @@
+use std::fmt::format;
+use std::time::Instant;
+
 use eframe::egui;
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([800.0, 600.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([1024.0, 768.0]),
         ..Default::default()
     };
     eframe::run_native(
         "Kalman Visualizer",
         options,
-        Box::new(|cc| Ok(Box::<KalmanVisualizer>::default())),
+        Box::new(|_cc| Ok(Box::<KalmanVisualizer>::default())),
     )
 }
 
+#[derive(Default)]
 struct KalmanVisualizer {
-    name: String,
-    age: u32,
-}
-
-impl Default for KalmanVisualizer {
-    fn default() -> Self {
-        Self {
-            name: String::from("Zort"),
-            age: 31,
-        }
-    }
+    selected_tab: usize,
 }
 
 impl KalmanVisualizer {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
     }
 }
 
 impl eframe::App for KalmanVisualizer {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ui, |ui| {
-            ui.heading("DENEME");
+        egui::Panel::top("top_panel").show(ui, |ui| {
             ui.horizontal(|ui| {
-                let name_label = ui.label("Your Name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
+                ui.heading("Uçuş Bilgisayarı");
+                ui.separator();
+                ui.selectable_value(&mut self.selected_tab, 0, "3D View");
+                ui.selectable_value(&mut self.selected_tab, 1, "Charts");
+                ui.selectable_value(&mut self.selected_tab, 2, "Logs");
             });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("Age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            if ui.button("Decrement").clicked() {
-                self.age -= 1;
-            }
-            ui.label(format!("Hello {} -> your age {}", self.name, self.age));
+        });
+        egui::Panel::left("left_panel")
+            .resizable(true)
+            .default_size(200.)
+            .show(ui, |ui| {
+                ui.label("Filter Controls");
+                ui.separator();
+            });
+
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.label(format!(
+                "Tab: {}",
+                ["3D View", "Charts", "Logs"][self.selected_tab]
+            ));
         });
     }
 }
